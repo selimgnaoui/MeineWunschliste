6b974f3b567830b8c364dcabd595815b30895a66
André Schume <schume@schumevision.de>
2019-05-16 17:50:18 +0200

    Start

--- footer.php
+++ footer.php
@@ -4,16 +4,16 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-4 card">
-                <h5 class="card-title">Footer 1</h5>
-                <p>Spalte 1</p>
+                <h5 class="card-title">Wer sind wir?</h5>
+                <p>Studenten mit einem Projekt für unser Fach Webprogrammierung.</p>
             </div>
             <div class="col-lg-4 card">
-                <h5 class="card-title">Footer 2</h5>
-                <p>Spalte 2</p>
+                <h5 class="card-title">Impressum</h5>
+                <p>Blubb blubb blubbediblub und blapp blupp blirb blarb</p>
             </div>
             <div class="col-lg-4 card">
-                <h5 class="card-title">Footer 3</h5>
-                <p>Spalte 3</p>
+                <h5 class="card-title">Kontakt</h5>
+                <p>Prinzipiell sind wir nie erreichbar, aber wir machen hier mal eine Ausnahme.</p>
             </div>
         </div>
     </div>
--- header.php
+++ header.php
@@ -1,6 +1,34 @@
 <header>
 
     <!-- HEADER INHALT -->
-    HEADER INHALT
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-primary">
+        <a class="navbar-brand" href="#">meineWunschliste</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Liste erzeugen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Liste suchen</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Login
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Login</a>
+                        <a class="dropdown-item" href="#">Register</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
 </header>
