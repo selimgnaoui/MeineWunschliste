38a5f99f1cd6fb1a0ac126db8d15a59884fb3954
Kleingehirsche <schume@schumevision.de>
2019-05-21 17:19:50 +0200

    Aufgeräumt zur Kollaboration

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-primary">
         <a class="navbar-brand" href="#">meineWunschliste</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" href="index.php">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -23,8 +23,8 @@
                         Login
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="Login.php">Login</a>
-                        <a class="dropdown-item" href="#">Register</a>
+                        <a class="dropdown-item" href="login.php">Login</a>
+                        <a class="dropdown-item" href="registration.php">Register</a>
                     </div>
                 </li>
             </ul>
