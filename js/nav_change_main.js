909474abc486fb2e436ded651a287c4288f0b9cc
Kleingehirsche <schume@schumevision.de>
2019-05-21 17:06:15 +0200

    2 - Versuch main auszutauschen...

--- js/nav_change_main.js
+++
@@ -1,7 +0,0 @@
-function navChange(site) {
-    if (site="makelist") {
-        var seite = document.getElementById("container");
-        seite.innerHTML = '<?php\ninclude('main.php');\n?>';
-
-    }
-}
--- js/startup.js
+++ js/startup.js
@@ -2,18 +2,6 @@ function startmeldung() {
     // alert('Cookie und DVSGO Meldung\n \nUnd hier kommt der passende Text rein.\nSind Sie damit einverstanden, drücken Sie auf den Button.\nAnsonsten verlassen Sie bitte die Seite.');
 }
 
-function switchCreateList() {
-    // alert("switchlist aktiviert");
-    var seite = document.getElementById("content");
-    // alert(seite);
-    // var neueSeite = "<?php include('main_start_loggedIn.php'); ?>";
-    var neueSeite = 'main2.html';
-    alert(neueSeite);
-    seite.innerHTML ="";
-    seite.innerHTML = neueSeite;
-
-}
-
 window.onload=()=> {
     startmeldung();
 }
