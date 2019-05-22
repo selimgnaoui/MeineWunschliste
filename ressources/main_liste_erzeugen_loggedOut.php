9b25d7777ba391203fac33dc4525a1f2900c2aca
Kleingehirsche <schume@schumevision.de>
2019-05-22 16:01:57 +0200

    NAV: Wenn eingeloggt -> logout.php; Wenn nicht -> login.php

--- ressources/main_liste_erzeugen_loggedOut.php
+++ ressources/main_liste_erzeugen_loggedOut.php
@@ -6,15 +6,15 @@
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h1 class="display-4">Liste erzeugen</h1>
-                        <p class="lead">Hier kommen informationen zu unserem Wunschlisten-System. Ganz viele Informationen, die hier rein geschrieben werden müssen. Eventuell machen wir auch noch ein paar Bilder rein, aber das haben wir noch nicht besprochen, als daß ich dazu schon eine Aussage machen könnte.</p>
+                        <p class="lead">Eine neue Liste wird erzeugt</p>
                     </div>
                 </div>
             </div>
             <div class="col">
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
-                        <h1 class="display-4">Liste erzeugen</h1>
-                        <p class="lead">Hier kommen informationen zu unserem Wunschlisten-System. Ganz viele Informationen, die hier rein geschrieben werden müssen. Eventuell machen wir auch noch ein paar Bilder rein, aber das haben wir noch nicht besprochen, als daß ich dazu schon eine Aussage machen könnte.</p>
+                        <h1 class="display-4">Vorhandene Listen:</h1>
+                        <p class="lead">Diese Listen sind bisher vorhanden.</p>
                     </div>
                 </div>
             </div>
--- ressources/main_login.php
+++ ressources/main_login.php
@@ -6,7 +6,19 @@
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h1 class="display-4">Login</h1>
-                        <p class="lead">Hier kommen informationen zu unserem Wunschlisten-System. Ganz viele Informationen, die hier rein geschrieben werden müssen. Eventuell machen wir auch noch ein paar Bilder rein, aber das haben wir noch nicht besprochen, als daß ich dazu schon eine Aussage machen könnte.</p>
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                <small id="emailHelp" class="form-text text-muted">Bitte benutzen Sie die Email-Adresse, mit der Sie sich registriert haben!</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                     </div>
                 </div>
             </div>
